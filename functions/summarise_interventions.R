a1989414c37c29de4228f746e8efcc2bcd22774a
seabbs <s.e.abbott12@gmail.com>
2020-03-14 22:42:32 +0000

    added data derived tables and dropped out of date tables

--- functions/summarise_interventions.R
+++ functions/summarise_interventions.R
@@ -1,10 +1,16 @@
 summarise_interventions <- function(interventions) {
   interventions %>% 
     dplyr::select(-date) %>% 
-    dplyr::group_by(country, intervention) %>% 
+    dplyr::group_by(country, type) %>% 
     dplyr::slice(1) %>% 
     dplyr::ungroup() %>% 
-    dplyr::count(type) %>% 
+    group_by(type) %>% 
+    dplyr::summarise(`Number of regions that have implemented` = dplyr::n(), 
+                     `National (%)` =  paste0(round(100 *sum(Scale %in% "National") / dplyr::n(), 1), "%"),
+                     `Enforced (%)` = paste0(round(100 *sum(Enforced %in% "yes") / dplyr::n(), 1), "%"),
+                      Regions = paste(country, collapse = ", ")) %>% 
+    dplyr::ungroup() %>% 
     tidyr::drop_na(type) %>% 
-    dplyr::arrange(desc(n)) 
+    dplyr::arrange(desc(`Number of regions that have implemented`)) %>% 
+    dplyr::rename(`Intervention type` = type)
 }
